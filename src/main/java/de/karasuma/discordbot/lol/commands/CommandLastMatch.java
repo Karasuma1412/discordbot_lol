c52ba6e0b431cc816c5c50ff80cb6780e5d700d0
Karasuma1412 <bjoerninorek@gmail.com>
2019-10-25 14:14:57 +0200

    implement Command interface in CommandLastMatch

--- src/main/java/de/karasuma/discordbot/lol/commands/CommandLastMatch.java
+++ src/main/java/de/karasuma/discordbot/lol/commands/CommandLastMatch.java
@@ -1,4 +1,26 @@
 package de.karasuma.discordbot.lol.commands;
 
-public class CommandLastMatch {
+import de.karasuma.discordbot.lol.commandhandling.Command;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+public class CommandLastMatch implements Command {
+    @Override
+    public boolean called(String[] args, MessageReceivedEvent event) {
+        return false;
+    }
+
+    @Override
+    public void action(String[] args, MessageReceivedEvent event) {
+
+    }
+
+    @Override
+    public void executed(boolean sucess, MessageReceivedEvent event) {
+
+    }
+
+    @Override
+    public String help() {
+        return null;
+    }
 }
